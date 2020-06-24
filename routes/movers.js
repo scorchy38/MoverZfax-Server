c904133a85e982761271fb13fc0c23c7ec030e1f
scorchy <vkumarsaraswat@gmail.com>
2020-06-24 06:41:34 +0530

    final heroku deploy

--- routes/movers.js
+++ routes/movers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/Post');
+const Post = require('../models/post');
 
 var mongodb = require('mongodb');
 
